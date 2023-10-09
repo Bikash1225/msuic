526e4a14a9e70c05fee8c2ee764a839ef7a4721e
٭Bιĸαѕн٭ <127423650+Bikash1225@users.noreply.github.com>
2023-10-09 08:06:52 +0530

    Update start.py

--- EllieXMusic/utils/inline/start.py
+++ EllieXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://te.legra.ph/file/cfb20cd24dc6c2448b7d2.jpg",
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://te.legra.ph/file/cfb20cd24dc6c2448b7d2.jpg"), 
         ],
     ]
     return buttons
